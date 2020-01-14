3cb45704f4f980a138a353c772a823338f227eb4
Robb Oliver <robb.oliver@progress.com>
2020-01-14 11:26:52 -0500

    Updating Gradle. Switching to TestNG.

--- src/test/java/com/rkoliver/net/swp/test/SWPTestUtils.java
+++ src/test/java/com/rkoliver/net/swp/test/SWPTestUtils.java
@@ -1,87 +1,80 @@
 package com.rkoliver.net.swp.test;
 
-import org.junit.Assert;
+import org.testng.Assert;
+
+import static org.testng.Assert.fail;
 
 public class SWPTestUtils {
 
-	public static void assertEquals(byte[] expected, int expectedOffset, int expectedLength, byte[] actual, int actualOffset) {
-		
-		if (expected == null && actual == null) {
-			
-			return;
-		}
 
-		if (expected == null && actual != null) {
-			
-			Assert.fail("Expected as null but actual was not.");
-		}
-		
-		if (expected != null && actual == null) {
-			
-			Assert.fail("Expected was not null but actual was.");
-		}
-		
-		for (int i = 0; i < expectedLength; i++) {
-			
-			Assert.assertEquals(expected[expectedOffset + i], actual[actualOffset + i]);
-		}
-	}
+    public static void assertEquals(byte[] actual, int actualOffset, byte[] expected, int expectedOffset, int expectedLength) {
 
-	public static void fail(String message) {
+        if (expected == null && actual == null) {
 
-		Assert.fail(message);
-	}
+            return;
+        }
 
-	public static <T> void assertEquals(T expected, T actual) {
+        if (expected == null && actual != null) {
 
-		Assert.assertEquals(expected, actual);
-	}
+            fail("Expected as null but actual was not.");
+        }
 
-	public static void println(byte[] value) {
-		
-		if (value == null) {
-			
-			println("<null>");
-		}
-		
-		print("byte[");
-		print(value.length);
-		print("] { ");
-		for (int i = 0; i < value.length; i++) {
-			
-			if (i > 0) {
-				
-				print(", ");
-			}
-			
-			print(value[i]);
-		}
-		println(" }");
-	}
+        if (expected != null && actual == null) {
 
-	private static void print(int value) {
-		
-		print(Integer.toString(value));
-	}
+            fail("Expected was not null but actual was.");
+        }
 
-	private static void print(byte value) {
+        for (int i = 0; i < expectedLength; i++) {
 
-		print(Byte.toString(value));
-	}
+            Assert.assertEquals(actual[actualOffset + i], expected[expectedOffset + i]);
+        }
+    }
 
-	private static void print(String value) {
 
-		System.out.print(value);
-	}
+    public static void println(byte[] value) {
 
-	static void println(String value) {
+        if (value == null) {
 
-		print(value);
-		print("\n");
-	}
+            println("<null>");
+        }
 
-	public static void fail(Exception e) {
+        print("byte[");
+        print(value.length);
+        print("] { ");
+        for (int i = 0; i < value.length; i++) {
 
-		fail(e.getMessage());
-	}
+            if (i > 0) {
+
+                print(", ");
+            }
+
+            print(value[i]);
+        }
+        println(" }");
+    }
+
+
+    private static void print(int value) {
+
+        print(Integer.toString(value));
+    }
+
+
+    private static void print(byte value) {
+
+        print(Byte.toString(value));
+    }
+
+
+    private static void print(String value) {
+
+        System.out.print(value);
+    }
+
+
+    static void println(String value) {
+
+        print(value);
+        print("\n");
+    }
 }
--- src/test/java/com/rkoliver/net/swp/test/SWPVarIntTest.java
+++ src/test/java/com/rkoliver/net/swp/test/SWPVarIntTest.java
@@ -1,147 +1,151 @@
 package com.rkoliver.net.swp.test;
 
-import org.junit.Test;
-
 import com.rkoliver.net.swp.SWPException;
 import com.rkoliver.net.swp.data.SWPUVarInt;
 import com.rkoliver.net.swp.util.New;
+import org.testng.annotations.Test;
 
-public class SWPVarIntTest { 
-	
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class SWPVarIntTest {
+
     @Test
-	public void test() {
-		
-    	try {
-    	
-    		byte[] bytes = New.bytes(36);
-    		bytes[0] = (byte)0x7F;
-    		
-    		bytes[1] = (byte)0xbf;
-    		bytes[2] = (byte)0xff;
-    		
-    		bytes[3] = (byte)0xdf;
-    		bytes[4] = (byte)0xff;
-    		bytes[5] = (byte)0xff;
-    		
-    		bytes[6] = (byte)0xef;
-    		bytes[7] = (byte)0xff;
-    		bytes[8] = (byte)0xff;
-    		bytes[9] = (byte)0xff;
-    		
-    		bytes[10] = (byte)0xf7;
-    		bytes[11] = (byte)0xff;
-    		bytes[12] = (byte)0xff;
-    		bytes[13] = (byte)0xff;
-    		bytes[14] = (byte)0xff;
+    public void test() {
 
-    		bytes[15] = (byte)0xfb;
-    		bytes[16] = (byte)0xff;
-    		bytes[17] = (byte)0xff;
-    		bytes[18] = (byte)0xff;
-    		bytes[19] = (byte)0xff;
-    		bytes[20] = (byte)0xff;
-    		
-    		bytes[21] = (byte)0xfd;
-    		bytes[22] = (byte)0xff;
-    		bytes[23] = (byte)0xff;
-    		bytes[24] = (byte)0xff;
-    		bytes[25] = (byte)0xff;
-    		bytes[26] = (byte)0xff;
-    		bytes[27] = (byte)0xff;
+        try {
 
-    		bytes[28] = (byte)0xfe;
-    		bytes[29] = (byte)0xff;
-    		bytes[30] = (byte)0xff;
-    		bytes[31] = (byte)0xff;
-    		bytes[32] = (byte)0xff;
-    		bytes[33] = (byte)0xff;
-    		bytes[34] = (byte)0xff;
-    		bytes[35] = (byte)0xff;
+            byte[] bytes = New.bytes(36);
+            bytes[0] = (byte) 0x7F;
 
-    		int[] outLength = New.ints(1);
-    		int length;
-    		byte[] actual = New.bytes(8);
-    		
-    		SWPTestUtils.assertEquals(127, SWPUVarInt.decodeInt(bytes, 0, 1));
-    		SWPTestUtils.assertEquals(127, SWPUVarInt.decodeInt(bytes, 0, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 1);
-    		length = SWPUVarInt.encodeInt(127, actual, 0, 1);
-    		SWPTestUtils.assertEquals(1, length);
-    		SWPTestUtils.assertEquals(bytes, 0, 1, actual, 0);
-    		
-    		SWPTestUtils.assertEquals(16383, SWPUVarInt.decodeInt(bytes, 1, 2));
-    		SWPTestUtils.assertEquals(16383, SWPUVarInt.decodeInt(bytes, 1, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 2);
-    		length = SWPUVarInt.encodeInt(16383, actual, 0, 2);
-    		SWPTestUtils.assertEquals(2, length);
-    		SWPTestUtils.assertEquals(bytes, 1, 2, actual, 0);
+            bytes[1] = (byte) 0xbf;
+            bytes[2] = (byte) 0xff;
 
-    		SWPTestUtils.assertEquals(2097151, SWPUVarInt.decodeInt(bytes, 3, 3));
-    		SWPTestUtils.assertEquals(2097151, SWPUVarInt.decodeInt(bytes, 3, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 3);
-    		length = SWPUVarInt.encodeInt(2097151, actual, 0, 3);
-    		SWPTestUtils.assertEquals(3, length);
-    		SWPTestUtils.assertEquals(bytes, 3, 3, actual, 0);
+            bytes[3] = (byte) 0xdf;
+            bytes[4] = (byte) 0xff;
+            bytes[5] = (byte) 0xff;
 
-    		SWPTestUtils.assertEquals(268435455, SWPUVarInt.decodeInt(bytes, 6, 4));
-    		SWPTestUtils.assertEquals(268435455, SWPUVarInt.decodeInt(bytes, 6, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 4);
-    		length = SWPUVarInt.encodeInt(268435455, actual, 0, 4);
-    		SWPTestUtils.assertEquals(4, length);
-    		SWPTestUtils.assertEquals(bytes, 6, 4, actual, 0);
-    		
-    		SWPTestUtils.assertEquals(34359738367L, SWPUVarInt.decodeLong(bytes, 10, 5));
-    		SWPTestUtils.assertEquals(34359738367L, SWPUVarInt.decodeLong(bytes, 10, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 5);
-    		length = SWPUVarInt.encodeLong(34359738367L, actual, 0, 5);
-    		SWPTestUtils.assertEquals(5, length);
-    		SWPTestUtils.assertEquals(bytes, 10, 5, actual, 0);
+            bytes[6] = (byte) 0xef;
+            bytes[7] = (byte) 0xff;
+            bytes[8] = (byte) 0xff;
+            bytes[9] = (byte) 0xff;
 
-    		SWPTestUtils.assertEquals(4398046511103L, SWPUVarInt.decodeLong(bytes, 15, 6));
-    		SWPTestUtils.assertEquals(4398046511103L, SWPUVarInt.decodeLong(bytes, 15, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 6);
-    		length = SWPUVarInt.encodeLong(4398046511103L, actual, 0, 6);
-    		SWPTestUtils.assertEquals(6, length);
-    		SWPTestUtils.assertEquals(bytes, 15, 6, actual, 0);
-    		
-    		SWPTestUtils.assertEquals(562949953421311L, SWPUVarInt.decodeLong(bytes, 21, 7));
-    		SWPTestUtils.assertEquals(562949953421311L, SWPUVarInt.decodeLong(bytes, 21, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 7);
-    		length = SWPUVarInt.encodeLong(562949953421311L, actual, 0, 7);
-    		SWPTestUtils.assertEquals(7, length);
-    		SWPTestUtils.assertEquals(bytes, 21, 7, actual, 0);
-    		
-    		SWPTestUtils.assertEquals(72057594037927935L, SWPUVarInt.decodeLong(bytes, 28, 8));
-    		SWPTestUtils.assertEquals(72057594037927935L, SWPUVarInt.decodeLong(bytes, 28, outLength));
-    		SWPTestUtils.assertEquals(outLength[0], 8);
-    		length = SWPUVarInt.encodeLong(72057594037927935L, actual, 0, 8);
-    		SWPTestUtils.assertEquals(8, length);
-    		SWPTestUtils.assertEquals(bytes, 28, 8, actual, 0);
-    		
-    		try {
-    			
-    			SWPUVarInt.decodeInt(bytes, 10, outLength);
-    			SWPTestUtils.fail("Expected exception but none was thrown.");
-    		}
-    		catch (SWPException e) { }
+            bytes[10] = (byte) 0xf7;
+            bytes[11] = (byte) 0xff;
+            bytes[12] = (byte) 0xff;
+            bytes[13] = (byte) 0xff;
+            bytes[14] = (byte) 0xff;
 
-    		try {
-    			
-    			SWPUVarInt.encodeLong(Long.MAX_VALUE, actual, 0, 8);
-    			SWPTestUtils.fail("Expected exception but none was thrown.");
-    		}
-    		catch (SWPException e) { }
+            bytes[15] = (byte) 0xfb;
+            bytes[16] = (byte) 0xff;
+            bytes[17] = (byte) 0xff;
+            bytes[18] = (byte) 0xff;
+            bytes[19] = (byte) 0xff;
+            bytes[20] = (byte) 0xff;
 
-    		SWPUVarInt.encodeInt(Integer.MAX_VALUE, actual, 0, 5);
-    		int i = SWPUVarInt.decodeInt(actual, 0, 5);
-    		SWPTestUtils.assertEquals(Integer.MAX_VALUE, i);
-    		
-    		SWPUVarInt varint = new SWPUVarInt(123L);
-    		SWPTestUtils.println(varint.bytesValue());    		
-    	}
-    	catch (SWPException e) {
-    		
-    		SWPTestUtils.fail(e.getMessage());
-    	}
-	}
+            bytes[21] = (byte) 0xfd;
+            bytes[22] = (byte) 0xff;
+            bytes[23] = (byte) 0xff;
+            bytes[24] = (byte) 0xff;
+            bytes[25] = (byte) 0xff;
+            bytes[26] = (byte) 0xff;
+            bytes[27] = (byte) 0xff;
+
+            bytes[28] = (byte) 0xfe;
+            bytes[29] = (byte) 0xff;
+            bytes[30] = (byte) 0xff;
+            bytes[31] = (byte) 0xff;
+            bytes[32] = (byte) 0xff;
+            bytes[33] = (byte) 0xff;
+            bytes[34] = (byte) 0xff;
+            bytes[35] = (byte) 0xff;
+
+            int[] outLength = New.ints(1);
+            int length;
+            byte[] actual = New.bytes(8);
+
+            assertEquals(SWPUVarInt.decodeInt(bytes, 0, 1), 127);
+            assertEquals(SWPUVarInt.decodeInt(bytes, 0, outLength), 127);
+            assertEquals(1, outLength[0]);
+            length = SWPUVarInt.encodeInt(127, actual, 0, 1);
+            assertEquals(length, 1);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 0, 1);
+
+            assertEquals(SWPUVarInt.decodeInt(bytes, 1, 2), 16383);
+            assertEquals(SWPUVarInt.decodeInt(bytes, 1, outLength), 16383);
+            assertEquals(2, outLength[0]);
+            length = SWPUVarInt.encodeInt(16383, actual, 0, 2);
+            assertEquals(length, 2);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 1, 2);
+
+            assertEquals(SWPUVarInt.decodeInt(bytes, 3, 3), 2097151);
+            assertEquals(SWPUVarInt.decodeInt(bytes, 3, outLength), 2097151);
+            assertEquals(3, outLength[0]);
+            length = SWPUVarInt.encodeInt(2097151, actual, 0, 3);
+            assertEquals(length, 3);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 3, 3);
+
+            assertEquals(SWPUVarInt.decodeInt(bytes, 6, 4), 268435455);
+            assertEquals(SWPUVarInt.decodeInt(bytes, 6, outLength), 268435455);
+            assertEquals(4, outLength[0]);
+            length = SWPUVarInt.encodeInt(268435455, actual, 0, 4);
+            assertEquals(length, 4);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 6, 4);
+
+            assertEquals(SWPUVarInt.decodeLong(bytes, 10, 5), 34359738367L);
+            assertEquals(SWPUVarInt.decodeLong(bytes, 10, outLength), 34359738367L);
+            assertEquals(5, outLength[0]);
+            length = SWPUVarInt.encodeLong(34359738367L, actual, 0, 5);
+            assertEquals(length, 5);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 10, 5);
+
+            assertEquals(SWPUVarInt.decodeLong(bytes, 15, 6), 4398046511103L);
+            assertEquals(SWPUVarInt.decodeLong(bytes, 15, outLength), 4398046511103L);
+            assertEquals(6, outLength[0]);
+            length = SWPUVarInt.encodeLong(4398046511103L, actual, 0, 6);
+            assertEquals(length, 6);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 15, 6);
+
+            assertEquals(SWPUVarInt.decodeLong(bytes, 21, 7), 562949953421311L);
+            assertEquals(SWPUVarInt.decodeLong(bytes, 21, outLength), 562949953421311L);
+            assertEquals(7, outLength[0]);
+            length = SWPUVarInt.encodeLong(562949953421311L, actual, 0, 7);
+            assertEquals(length, 7);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 21, 7);
+
+            assertEquals(SWPUVarInt.decodeLong(bytes, 28, 8), 72057594037927935L);
+            assertEquals(SWPUVarInt.decodeLong(bytes, 28, outLength), 72057594037927935L);
+            assertEquals(8, outLength[0]);
+            length = SWPUVarInt.encodeLong(72057594037927935L, actual, 0, 8);
+            assertEquals(length, 8);
+            SWPTestUtils.assertEquals(actual, 0, bytes, 28, 8);
+
+            try {
+
+                SWPUVarInt.decodeInt(bytes, 10, outLength);
+                fail("Expected exception but none was thrown.");
+            }
+            catch (SWPException e) {
+            }
+
+            try {
+
+                SWPUVarInt.encodeLong(Long.MAX_VALUE, actual, 0, 8);
+                fail("Expected exception but none was thrown.");
+            }
+            catch (SWPException e) {
+            }
+
+            SWPUVarInt.encodeInt(Integer.MAX_VALUE, actual, 0, 5);
+            int i = SWPUVarInt.decodeInt(actual, 0, 5);
+            assertEquals(i, Integer.MAX_VALUE);
+
+            SWPUVarInt varint = new SWPUVarInt(123L);
+            SWPTestUtils.println(varint.bytesValue());
+        }
+        catch (SWPException e) {
+
+            fail(e.getMessage());
+        }
+    }
 }
